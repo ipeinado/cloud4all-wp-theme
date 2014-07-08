4214b105ec27f08e6809b63d8581c3f11116c6ce
Ignacio Peinado <i.peinado.martinez@gmail.com>
2014-07-08 21:41:39 +0200

    added tweets

--- page-templates/front-page.php
+++ page-templates/front-page.php
@@ -2,8 +2,8 @@
 /**
  * Template Name: Front Page Template for the Cloud4all Website
  *
- * Description: This page is the homepage of the Cloud4all website. 
- * The front page template for Cloud4all comprises a set of dedicated 
+ * Description: This page is the homepage of the Cloud4all website.
+ * The front page template for Cloud4all comprises a set of dedicated
  * widgets and elements, aiming to become a proper landing page --
  * anything you'd like -- followed by front-page-only widgets in one or two columns.
  *
@@ -64,7 +64,7 @@ get_header(); ?>
           <a href="#">Go to news</a>
           <h2>News</h2>
         </header>
-        <ul class="news-list"> 
+        <ul class="news-list">
 
           <?php $the_query = new WP_Query( 'posts_per_page=4, category_name=news'); ?>
 
@@ -91,8 +91,67 @@ get_header(); ?>
           <h2>Tweets</h2>
           <a href="https://twitter.com/Cloud4all7FP" class="twitter-follow-button" data-show-count="false" data-lang="en">Follow @Cloud4all7FP</a>
         </header>
+        <div class="tweets-container">
+            <ul class="tweets">
+                <?php
+                    $tweets = getTweets("Cloud4all7FP", 3);
+
+                    foreach($tweets as $tweet) {
+                        if ($tweet['text']) {
+
+                            $the_tweet = $tweet['text'];
+
+                            // i. User_mentions must link to the mentioned user's profile.
+                            if(is_array($tweet['entities']['user_mentions'])){
+                                foreach($tweet['entities']['user_mentions'] as $key => $user_mention){
+                                    $the_tweet = preg_replace(
+                                        '/@'.$user_mention['screen_name'].'/i',
+                                        '<a href="http://www.twitter.com/'.$user_mention['screen_name'].'" target="_blank">@'.$user_mention['screen_name'].'</a>',
+                                        $the_tweet);
+                                }
+                            }
+
+                            // ii. Hashtags must link to a twitter.com search with the hashtag as the query.
+                            if(is_array($tweet['entities']['hashtags'])){
+                                foreach($tweet['entities']['hashtags'] as $key => $hashtag){
+                                    $the_tweet = preg_replace(
+                                    '/#'.$hashtag['text'].'/i',
+                                    '<a href="https://twitter.com/search?q=%23'.$hashtag['text'].'&src=hash" target="_blank">#'.$hashtag['text'].'</a>',
+                                    $the_tweet);
+                                }
+                            }
+
+                            // iii. Links in Tweet text must be displayed using the display_url
+                            //      field in the URL entities API response, and link to the original t.co url field.
+                            if(is_array($tweet['entities']['urls'])){
+                                foreach($tweet['entities']['urls'] as $key => $link){
+                                    $the_tweet = preg_replace(
+                                    '`'.$link['url'].'`',
+                                    '<a href="'.$link['url'].'" target="_blank">'.$link['url'].'</a>',
+                                    $the_tweet);
+                                }
+                            }
+
+                            echo '<li>' .
+                                    '<time>' .
+                                    date("M d \, y", strtotime($tweet['created_at'])).
+                                    '</time>'.
+                                    '<p>'.
+                                    $the_tweet.
+                                    '</p>'.
+                                    '<div class="twitter-intents">
+                                        <p><a class="reply" href="https://twitter.com/intent/tweet?in_reply_to='.$tweet['id_str'].'">Reply</a></p>
+                                        <p><a class="retweet" href="https://twitter.com/intent/retweet?tweet_id='.$tweet['id_str'].'">Retweet</a></p>
+                                        <p><a class="favorite" href="https://twitter.com/intent/favorite?tweet_id='.$tweet['id_str'].'">Favorite</a></p>
+                                    </div>'.
+                                     '</li>';
+                             }
+                        }
+
+                ?>
+        </div>
       </div>
-     
+
     </section> <!-- news-banner -->
 
 	</div><!-- #primary -->
