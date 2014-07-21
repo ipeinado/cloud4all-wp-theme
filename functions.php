992952fd1965a1ee8231fdc31cd6145e4a4138ea
Ignacio Peinado <i.peinado.martinez@gmail.com>
2014-07-21 16:29:50 +0200

    added support for IE lower than 9

--- functions.php
+++ functions.php
@@ -8,11 +8,8 @@ function cloud4all_dequeue_styles() {
 
 add_action( 'wp_enqueue_styles', 'cloud4all_dequeue_styles', 11);
 
-function cloud4all_enqueue_styles() {
-	global $wp_styles;
-	wp_enqueue_style('cloud4all-ie', get_stylesheet_directory_uri() . "/css/ie.css", array("twentytwelve-style"), '1.0');
-	$wp_styles->add_data('cloud4all-ie', 'conditional', 'lt IE 9');
-}
+wp_enqueue_style('cloud4all-ie', get_stylesheet_directory_uri() . "/css/ie.css", array("twentytwelve-style"), '1.0');
+$wp_styles->add_data('cloud4all-ie', 'conditional', 'lt IE 9');
 
 function cloud4all_scripts() {
 
--- header.php
+++ header.php
@@ -37,7 +37,6 @@
 <?php // Loads HTML5 JavaScript file to add support for HTML5 elements in older IE versions. ?>
 <!--[if lt IE 9]>
 <script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
-<link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_directory(); ?>/css/ie.css" />
 <![endif]-->
 
 <?php wp_head(); ?>
